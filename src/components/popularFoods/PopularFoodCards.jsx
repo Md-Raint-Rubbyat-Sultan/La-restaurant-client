bdaaf7344b0ee234526f764b8f1e952f2af717ce
md ranit rubbyat sultan <mdranitrubbyatsultan@gmail.com>
2023-11-06 22:14:23 +0600

    popular food card section is ready

---
+++ src/components/popularFoods/PopularFoodCards.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+const PopularFoodCards = ({ food }) => {
+  const { name, category, img, price } = food;
+  return (
+    <div className="overflow-hidden bg-base-100 shadow-xl border-2 border-gray-300 rounded-lg">
+      <figure>
+        <img className="w-full h-56 md:h-72 lg:h-80" src={img} alt={name} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>Category: {category}</p>
+        <p>Price: ${price}</p>
+        <div className="card-actions justify-center">
+          <button className="border-2 border-gray-300 hover:text-white hover:bg-black mt-10 px-6 py-2 font-semibold text-lg rounded-lg">
+            Details
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+PopularFoodCards.propTypes = {
+  food: PropTypes.object.isRequired,
+};
+
+export default PopularFoodCards;
--- src/components/popularFoods/PopularFoods.jsx
+++ src/components/popularFoods/PopularFoods.jsx
@@ -1,7 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSucre from "../../hooks/useAxiosSucre";
+import Spinner from "../Spinner/Spinner";
+import { toast } from "react-hot-toast";
+import PopularFoodCards from "./PopularFoodCards";
+
 const PopularFoods = () => {
+  const url = useAxiosSucre();
+
+  const {
+    isPending,
+    isError,
+    error,
+    data: foods,
+  } = useQuery({
+    queryKey: ["popular-foods"],
+    queryFn: () => url.get("/popular-foods").then((res) => res.data),
+  });
+
+  if (isPending) return <Spinner />;
+
+  if (isError) return toast.error(error.message);
+
+  console.log(foods);
+
   return (
-    <div>
-      <p>popular</p>
+    <div className="container mx-auto px-4 xl:px-0 my-28 xl:my-40 space-y-20">
+      <h3 className="text-center text-5xl font-semibold">Popular in Menu</h3>
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-12">
+        {foods?.map((food) => (
+          <PopularFoodCards key={food?._id} food={food} />
+        ))}
+      </div>
     </div>
   );
 };
